8e1f1b4b6fad1b518882589f44dcd1aa2db5923a
WTNB1225 <144193938+WTNB1225@users.noreply.github.com>
2025-03-27 02:14:25 +0900

    Add get started page

---
+++ lib/presentation/intro/pages/get_started.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:spotify_clone/common/widgets/button/basic_app_button.dart';
+import 'package:spotify_clone/core/configs/theme/app_colors.dart';
+import 'package:spotify_clone/core/configs/theme/assets/app_images.dart';
+import 'package:spotify_clone/core/configs/theme/assets/app_vectors.dart';
+
+class GetStartedPage extends StatelessWidget {
+  const GetStartedPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    debugPrint(AppImages.introBG);
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 40, vertical: 40),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage(AppImages.introBG),
+              ),
+            ),
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: SvgPicture.asset(AppVectors.logo),
+                ),
+                Spacer(flex: 5),
+                Text(
+                  'Enjoy Listening To Music',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(height: 21),
+                Text(
+                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sagittis enim purus sed phasellus. Cursus ornare id scelerisque aliquam.',
+                  style: TextStyle(
+                    fontSize: 13,
+                    fontWeight: FontWeight.w500,
+                    color: AppColors.grey,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                const SizedBox(height: 20),
+                BasicAppButton(onPressed: () {}, title: 'Get Started'),
+                Spacer(flex: 1),
+              ],
+            ),
+          ),
+          Container(color: Colors.black.withValues(alpha: 0.15)),
+        ],
+      ),
+    );
+  }
+}
