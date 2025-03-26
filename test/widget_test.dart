7bd442282784cad50fda82a83786f43af428499d
WTNB1225 <144193938+WTNB1225@users.noreply.github.com>
2025-03-26 22:51:46 +0900

    fix test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,1 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:spotify_clone/main.dart';
 
+void main() {
+  testWidgets('MyApp displays Hello World', (WidgetTester tester) async {
+    // アプリをウィジェットツリーに追加
+    await tester.pumpWidget(const MyApp());
+  });
+}
